35c6c1bd70e5c697af43bd31dcb6f6d017f01ad4
Austin Bachurski <austinbachurski@gmail.com>
2025-09-01 12:24:38 -0600

    Order and format file, add comments.

--- src/AVLTree/Demo/AVLTreeDemo.java
+++ src/AVLTree/Demo/AVLTreeDemo.java
@@ -1,24 +1,26 @@
 package AVLTree.Demo;
 
 import AVLTree.AVLTree;
-
 import java.util.Scanner;
 
 public class AVLTreeDemo {
+    // Handle for the tree data structure.
     AVLTree tree = null;
 
+    // Scanner to read from the console.
+    Scanner stdin = new Scanner(System.in);
+
+    // Begins the main loop of the demo.
     public void start() {
         System.out.println("Welcome to the interactive demo for my AVL Tree implementation.");
 
         printMenu();
 
-        String prompt = "Please make a numeric selection: ";
-
         while (true)
         {
-            switch (readNumberFromConsole(prompt))
+            switch (readNumberFromConsole("Please make a numeric selection: "))
             {
-                //Create a binary search tree
+                // Create a binary search tree.
                 case 1:
                     if (tree == null) {
                         createTree();
@@ -30,7 +32,7 @@ public class AVLTreeDemo {
                     }
                     break;
 
-                //Add a node
+                // Add a node.
                 case 2:
                     if (tree == null) {
                         warnNull();
@@ -39,7 +41,7 @@ public class AVLTreeDemo {
                     addNode();
                     break;
 
-                //Delete a node
+                // Delete a node.
                 case 3:
                     if (tree == null) {
                         warnNull();
@@ -48,7 +50,7 @@ public class AVLTreeDemo {
                     deleteNode();
                     break;
 
-                //Print nodes by InOrder
+                // Print nodes by InOrder.
                 case 4:
                     if (tree == null) {
                         warnNull();
@@ -57,7 +59,7 @@ public class AVLTreeDemo {
                     printInOrder();
                     break;
 
-                //Print nodes by PreOrder
+                // Print nodes by PreOrder.
                 case 5:
                     if (tree == null) {
                         warnNull();
@@ -66,7 +68,7 @@ public class AVLTreeDemo {
                     printPreOrder();
                     break;
 
-                //Print nodes by PostOrder
+                // Print nodes by PostOrder.
                 case 6:
                     if (tree == null) {
                         warnNull();
@@ -75,10 +77,11 @@ public class AVLTreeDemo {
                     printPostOrder();
                     break;
 
-                //Exit program
+                // Exit program.
                 case 7:
                     return;
 
+                // Request a valid number and read input again.
                 default:
                     System.out.println("Please make a valid selection, try again.");
                     continue;
@@ -87,11 +90,7 @@ public class AVLTreeDemo {
         }
     }
 
-    private  void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
-
+    // Request and validate a number from the console and add it to the tree.
     private void addNode() {
         clearScreen();
         int value = readNumberFromConsole("Enter the value you wish to add: ");
@@ -105,15 +104,34 @@ public class AVLTreeDemo {
 
         tree.insert(value);
 
-        clearScreen();
+        //clearScreen();
         System.out.println("Value " + value + " inserted into the tree.\n");
     }
 
+    // Clear the console.
+    private  void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    // If the user tries to create a tree when one already exists, warn that
+    // data will be lost and confirm before destroying the existing tree.
     private boolean confirmDestroy() {
         System.out.println("A tree already exists, if you create a new one, data will be lost.");
         return readBoolFromConsole("Destroy it? (y/n): ");
     }
 
+    // Create a new tree and populate it with { 1, 2, 3, 4, 5, 6, 7 } per requirements.
+    private void createTree() {
+        clearScreen();
+
+        tree = new AVLTree();
+        tree.populate();
+
+        System.out.println("New tree created and populated.\n");
+    }
+
+    // Request and validate a number from the console and remove it from the tree.
     private void deleteNode() {
         clearScreen();
         int value = readNumberFromConsole("Enter the value you wish to remove: ");
@@ -131,34 +149,39 @@ public class AVLTreeDemo {
         System.out.println("Value " + value + " removed from the tree.\n");
     }
 
+    // Displays the elements of the tree in "InOrder" traversal.
     private void printInOrder() {
         clearScreen();
         tree.printNodesByInOrder();
     }
 
-    private void printPreOrder() {
-        clearScreen();
-        tree.printNodesByPreOrder();
+    // Displays the operation menu.
+    private  void printMenu() {
+        System.out.println("What would you like to do?");
+        System.out.println("1. Create a binary search tree");
+        System.out.println("2. Add a node");
+        System.out.println("3. Delete a node");
+        System.out.println("4. Print nodes by InOrder");
+        System.out.println("5. Print nodes by PreOrder");
+        System.out.println("6. Print nodes by PostOrder");
+        System.out.println("7. Exit program");
     }
 
+    // Displays the elements of the tree in "PostOrder" traversal.
     private void printPostOrder() {
         clearScreen();
         tree.printNodesByPostOrder();
     }
 
-    private void createTree() {
+    // Displays the elements of the tree in "PreOrder" traversal.
+    private void printPreOrder() {
         clearScreen();
-
-        tree = new AVLTree();
-        tree.populate();
-
-        System.out.println("New tree created and populated.\n");
+        tree.printNodesByPreOrder();
     }
 
+    // Read a valid yes/no from the console and return it's boolean equivalent.
     private boolean readBoolFromConsole(String prompt) {
         System.out.println(prompt);
-
-        Scanner stdin = new Scanner(System.in);
 
         while (true) {
             String input = stdin.nextLine();
@@ -176,9 +199,9 @@ public class AVLTreeDemo {
         }
     }
 
+    // Read a valid number from the console and return said number.
     private int readNumberFromConsole(String prompt) {
         System.out.print(prompt);
-        Scanner stdin = new Scanner(System.in);
 
         while (true) {
             String input = stdin.nextLine();
@@ -191,18 +214,9 @@ public class AVLTreeDemo {
         }
     }
 
-    private  void printMenu() {
-        System.out.println("What would you like to do?");
-        System.out.println("1. Create a binary search tree");
-        System.out.println("2. Add a node");
-        System.out.println("3. Delete a node");
-        System.out.println("4. Print nodes by InOrder");
-        System.out.println("5. Print nodes by PreOrder");
-        System.out.println("6. Print nodes by PostOrder");
-        System.out.println("7. Exit program");
-    }
-
+    // Warns the user that the tree does not exist if it has not been created yet.
     private  void warnNull() {
         System.out.println("Please create a tree before trying to interact with said tree.");
     }
 }
+
