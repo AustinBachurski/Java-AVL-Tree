be31aa53aac32f3f8e80bfc914692c48a8bfd46d
Austin Bachurski <austinbachurski@gmail.com>
2025-09-01 12:24:52 -0600

    Add comments and finalize formatting.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import AVLTree.Demo.AVLTreeDemo;
 
 public class Main {
+    // Program entry point.
     public static void main(String[] args) {
+
+        // Initialize the demo object and start the demo.
         AVLTreeDemo demo = new AVLTreeDemo();
         demo.start();
     }
 }
+
